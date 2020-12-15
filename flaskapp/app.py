9af815999856c4a750efdcfbbb0f63081ab712bf
Genevieve Sloup <genevievesloup@MacBook-Pro-60.local>
2020-12-14 22:13:10 -0500

    working flask app jsonify

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -31,10 +31,10 @@ Base.prepare(engine, reflect=True)
 # Save references to each table
 players = Base.classes.player_table
 
-# print(players)
+print(players)
 
-# Create our session (link) from Python to the DB
-session = Session(engine)
+# # Create our session (link) from Python to the DB 
+# session = Session(engine)
 
 #################################################
 # Flask Routes
@@ -43,41 +43,12 @@ session = Session(engine)
 @app.route("/")
 def index():
     """Return the homepage."""
-    return (
-        f"Available Routes:<br/>"
-        f"/players"
-    )
+    return render_template("index.html")
 
 @app.route("/players")
-def stats():
-    """Return a list player data"""
-    # Query all players
-    sel = [
-        players.short_name,
-        players.age,
-        players.club,
-        # players.AGE,
-        # players.LOCATION,
-        # players.BBTYPE,
-        # players.WFREQ,
-    ]
-    results = session.query(*sel).all()
-
-    session.close()
-
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_players = []
-    import base64
-    for short_name, age, club in results:
-        encoded = base64.b64encode(b'{}'.format(short_name)) 
-        player_dict = {}
-        player_dict["short_name"] = encoded.decode('ascii') 
-        player_dict["age"] = age
-        player_dict["club"] = club
-        all_players.append(player_dict)
-    
-    return jsonify(all_players)
-
+def players():
+    """Return the homepage."""
+    return "players"
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ init.py
@@ -0,0 +1,3 @@
+from flask_app.app import db 
+
+db.create_all()
