184a316c593239255a68274a8425b3d88396f2a8
Joshua Sohan <joshuasohan@Joshuas-MBP.fios-router.home>
2020-12-20 21:25:28 -0500

    new push
    !#

--- assets/js/chloropleth.js
+++ assets/js/chloropleth.js
@@ -1,4 +1,5 @@
-Plotly.d3.csv('/data/top_players.csv', function(err, rows){
+Plotly.d3.csv('/old/player_location.csv', function(err, rows){
+//Plotly.d3.csv('/data/top_players.csv', function(err, rows){
       function unpack(rows, key) {
           return rows.map(function(row) { return row[key]; });
       }
@@ -6,9 +7,12 @@ Plotly.d3.csv('/data/top_players.csv', function(err, rows){
     var data = [{
         type: 'choropleth',
         locationmode: 'country names',
-        locations: unpack(rows, 'nationality'),
-        z: unpack(rows, 'wage_eur'),
-        text: unpack(rows, 'short_name'),
+        locations: unpack(rows, 'country'),
+        //locations: unpack(rows, 'nationality'),
+        z: unpack(rows, 'nationality'),
+        //z: unpack(rows, 'wage_eur'),
+        text: unpack(rows, 'country'),
+        //text: unpack(rows, 'short_name'),
         autocolorscale: true
     }];
 
